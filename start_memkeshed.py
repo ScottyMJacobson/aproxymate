20e24d6be2a94c619587f593519d0fcf1ac229f3
Scotty Jacobson <ScottyMJacobson@gmail.com>
2015-04-24 14:09:12 -0400

    Rearrange start_memkeshed to just start up the memkeshed daemon on the given port

--- start_memkeshed.py
+++ start_memkeshed.py
@@ -10,20 +10,18 @@ from memkeshed import memkeshed
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("port", type=int ,help="Port number to bind proxy to")
+    parser.add_argument("memkeshed_port", type=int ,help="Port number to bind proxy to")
     args = parser.parse_args()
 
-    address_struct = ("127.0.0.1", args.port)
+    address_struct = ("127.0.0.1", args.memkeshed_port)
     mk_server = memkeshed.MemKeshedServer(address_struct)
     mk_daemon = threading.Thread(target=mk_server.serve_forever)
     mk_daemon.setDaemon(True)
     mk_daemon.start()
 
-    mk_client = memkeshed.MemKeshedClient(args.port)
+    #mk_client = memkeshed.MemKeshedClient(args.memkeshed_port)
 
     while True:
-        print mk_client.put_key("test_key", "the_test_value", 1000)
-        print mk_client.get_key("test_key")
         time.sleep(10)
 
 
